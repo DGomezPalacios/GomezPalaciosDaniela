022ce81d36db05004a5416d96303a35030eb4e4d
Dgomez <dgomez@acl.cl>
2024-07-17 19:34:30 -0400

    clasif

--- PN_GOMEZDANIELA_FPY1101_0012V/PN_GOMEZDANIELA_FPY11010012V.py
+++ PN_GOMEZDANIELA_FPY1101_0012V/PN_GOMEZDANIELA_FPY11010012V.py
@@ -5,14 +5,41 @@ import math
 #La aplicación debe permitir analizar los sueldos de 10 empleados, los cuales para efectos de este prototipo se crearán de
 #forma aleatoria entre $300.000 y $2.500.000. Utilice la siguiente lista para asignar los sueldos a cada empleado
 
-#Listas para guardar empleados y sueldos
-empleados = ["Juan Pérez", "María García", "Carlos López", "Ana Martínez", "Pedro Rodríguez", "Laura Hernández", "Miguel Sánchez", "Isabel Gómez", "Francisco Díaz", "Elena Fernández"]
+#Listas para guardar trabajadores y sueldos
+trabajadores = ["Juan Pérez", "María García", "Carlos López", "Ana Martínez", "Pedro Rodríguez", "Laura Hernández", "Miguel Sánchez", "Isabel Gómez", "Francisco Díaz", "Elena Fernández"]
 sueldos = []
+
+#Menú
+def menu():
+    while True:
+        print("\nMenú:")
+        print("1.- Asignar sueldos aleatorios")
+        print("2.- Clasificar sueldos")
+        print("3.- Ver estadísticas")
+        print("4.- Reporte de sueldos")
+        print("5.- Salir del programa")
+
+        #Leer la opcion del usuario
+        opcion = input("Seleccione una opción: ")
+
+        #Ejecutar lo que leigió el usuairo
+        if opcion == '1':
+            generar_sueldos()
+        elif opcion == '2':
+            clasificar_sueldos()
+
+
 
 #Función para asignar sueldos aleatorios
 
 def generar_sueldos():
-    for _ in range(10):
-        sueldos.append(random.randint(300000, 2500000))
+    sueldos = [random.randint(300000, 2500000) for _ in range(10)]
+    print("Sueldos asignados:", sueldos)
 
+#Funcion para clasificar sueldos
+
+def clasificar_sueldos():
+    sueldos_ordenados = sorted(sueldos)
+    mediana = sueldos_ordenados[len(sueldos_ordenados) // 2]
+    print("Sueldo medio:", mediana)
 
